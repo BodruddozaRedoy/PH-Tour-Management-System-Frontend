af91d23d56d6f4b14bce527db4c8d6c4d1279d70
bodruddozaredoy <bodruddozaredoy@gmail.com>
2025-08-18 04:40:11 +0600

    d

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -36,7 +36,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
               <SidebarMenu>
                 {item.items.map((item) => (
                   <SidebarMenuItem key={item.title}>
-                    <SidebarMenuButton asChild isActive={item.isActive}>
+                    <SidebarMenuButton asChild>
                       <Link to={item.url}>{item.title}</Link>
                     </SidebarMenuButton>
                   </SidebarMenuItem>
